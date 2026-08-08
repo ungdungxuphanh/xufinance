401ff0ec2aa9c557ed1621d25e3e7edda8077178
ungdungxuphanh <ungdungxuphanh@gmail.com>
2026-08-08 13:13:24 +0700

    .

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xufinance.app',
+  appName: 'Xu Finance',
+  webDir: '.output/public'
+};
+
+export default config;
