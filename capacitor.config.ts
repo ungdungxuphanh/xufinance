16e24b05c5fa8ce1d591fe32577ed981a4cd6330
ungdungxuphanh <ungdungxuphanh@gmail.com>
2026-08-08 13:28:11 +0700

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.xufinance.app',
   appName: 'Xu Finance',
-  webDir: '.output/public'
+  webDir: '.output/public',
+  server: {
+    url: 'https://xufinance.netlify.app', // Trỏ trực tiếp về trang Netlify
+    cleartext: true
+  }
 };
 
 export default config;
